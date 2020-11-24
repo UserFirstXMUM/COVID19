8f56047e548604e997c1830233d073ad67e18b38
Tangdd-abc <1286958333@qq.com>
2020-11-24 19:34:59 +0800

    edit_page,login_page, register_page, info_page and qr_page finished by tang

--- app/src/main/java/com/example/covid19/LoginPage.java
+++ app/src/main/java/com/example/covid19/LoginPage.java
@@ -2,13 +2,55 @@ package com.example.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class LoginPage extends AppCompatActivity {
+public class LoginPage<UserDBHelper> extends AppCompatActivity {
+    private EditText UserName;
+    private EditText Password;
+    private EditText Login;
+    private EditText Register;
+    private TextView Info;
+    private int Counter;
+    private UserDBHelper mHelper;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        UserName = findViewById(R.id.et_username);
+        Password = findViewById(R.id.et_password1);
+        Login = findViewById(R.id.loginbtn);
+        Register = findViewById((R.id.register));
+        Info = findViewById(R.id.info);
+        Info.setText("No of attempts remaining: 5");
+        Counter = 5;
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, MainActivity.class);
+                Bundle bundle=new Bundle();
+                intent.putExtras(bundle);
+                startActivity(intent);
+                }
+        });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/covid19/RegisterPage.java
+++ app/src/main/java/com/example/covid19/RegisterPage.java
@@ -1,14 +1,124 @@
 package com.example.covid19;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class RegisterPage extends AppCompatActivity {
+public class RegisterPage extends AppCompatActivity implements OnClickListener {
+    private EditText et_name;
+    private EditText et_age;
+    private EditText et_email;
+    private EditText et_phone;
+    private EditText et_password1;
+    private EditText et_password2;
+    private EditText et_gender;
+    private EditText et_birth;
+    private EditText registerbtn;
+    private boolean manager = false;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        et_name = findViewById(R.id.et_username);
+        et_age = findViewById(R.id.et_age);
+        et_email = findViewById(R.id.et_email);
+        et_phone = findViewById(R.id.et_phone);
+        et_password1 = findViewById(R.id.et_password1);
+        et_password2 = findViewById(R.id.et_password2);
+        et_gender = findViewById(R.id.et_gender);
+        et_birth = findViewById(R.id.et_birth);
+        registerbtn = findViewById(R.id.register_btn);
+        findViewById(R.id.register_btn).setOnClickListener(this);
+        initTypeSpinner();
     }
+
+    // Initializes the drop-down box
+    private void initTypeSpinner() {
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<String>(this,
+                R.layout.item_select, typeArray);
+        typeAdapter.setDropDownViewResource(R.layout.item_dropdown);
+        Spinner sp_married = findViewById(R.id.sp_married);
+        sp_married.setPrompt("Please select");
+        sp_married.setAdapter(typeAdapter);
+        sp_married.setSelection(0);
+        sp_married.setOnItemSelectedListener(new TypeSelectedListener());
+    }
+
+    private String[] typeArray = {"User", "Manager"};
+    class TypeSelectedListener implements OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            manager = (arg2 == 0) ? false : true;
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.register_btn) {
+            String name = et_name.getText().toString();
+            String age = et_age.getText().toString();
+            String email = et_email.getText().toString();
+            String phone = et_phone.getText().toString();
+            String password1 = et_password1.getText().toString();
+            String password2 = et_password2.getText().toString();
+            String gender = et_gender.getText().toString();
+            Boolean g=false;
+            if (gender == "Male")
+                g = true;
+            else
+                g = false;
+            String birth = et_birth.getText().toString();
+            if (TextUtils.isEmpty(name)) {
+                showToast("First fill name please");
+                return;
+            } else if (TextUtils.isEmpty(age)) {
+                showToast("First fill age please");
+                return;
+            } else if (TextUtils.isEmpty(gender)) {
+                showToast("First fill gender please");
+                return;
+            } else if (TextUtils.isEmpty(email)) {
+                showToast("First fill email please");
+                return;
+            }else if (TextUtils.isEmpty(phone)) {
+                showToast("First fill phone number please");
+                return;
+            }else if (TextUtils.isEmpty(password1)) {
+                showToast("First fill password please");
+                return;
+            }else if (TextUtils.isEmpty(birth)) {
+                showToast("First fill birthday please");
+                return;
+            }else if (TextUtils.isEmpty(password2)) {
+                showToast("First fill password please");
+                return;
+            }else if (!password1.equals(password2)) {
+                showToast("Please make sure that the password input is consistent");
+                return;
+            }
+
+            showToast("Information has been written to the SQLite database");
+            Intent intent = new Intent(this, LoginPage.class);
+            startActivity(intent);
+        }
+    }
+
+    private void showToast(String desc) {
+        Toast.makeText(this, desc, Toast.LENGTH_SHORT).show();
+    }
+
 }
